e9f3c9dfd0350e3585b3317fc6f5b91871793365
DevOrc <ncharlton002@gmail.com>
2020-04-07 16:01:14 -0400

    Redo item duct rendering

--- core/src/com/noahcharlton/robogeddon/block/duct/ItemDuct.java
+++ core/src/com/noahcharlton/robogeddon/block/duct/ItemDuct.java
@@ -1,6 +1,5 @@
 package com.noahcharlton.robogeddon.block.duct;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.noahcharlton.robogeddon.Core;
@@ -11,13 +10,12 @@ import com.noahcharlton.robogeddon.world.World;
 
 public class ItemDuct extends Block implements BlockRenderer {
 
-    private static Color innerBorder = new Color(0x7E0101FF);
-    private static Color outerBorder = new Color(0x403F3FFF);
-    private static Color bottom = new Color(0x6B6B6BFF);
-    private static Color glassColor = new Color(0x207EB554);
-
     private static boolean textureRegistered = false;
-    private static TextureRegion texture;
+    private static TextureRegion centerTexture;
+    private static TextureRegion topTexture;
+    private static TextureRegion rightTexture;
+    private static TextureRegion leftTexture;
+    private static TextureRegion bottomTexture;
 
     private final Direction direction;
 
@@ -32,60 +30,40 @@ public class ItemDuct extends Block implements BlockRenderer {
         this.renderer = this;
 
         if(!textureRegistered){
-            Core.assets.registerTexture("blocks/item_duct").setOnLoad(t -> texture = t);
+            Core.assets.registerTexture("blocks/item_duct").setOnLoad(t -> centerTexture = t);
+            Core.assets.registerTexture("blocks/item_duct_top").setOnLoad(t -> topTexture = t);
+            Core.assets.registerTexture("blocks/item_duct_right").setOnLoad(t -> rightTexture = t);
+            Core.assets.registerTexture("blocks/item_duct_left").setOnLoad(t -> leftTexture = t);
+            Core.assets.registerTexture("blocks/item_duct_bottom").setOnLoad(t -> bottomTexture = t);
             textureRegistered = true;
         }
     }
 
     @Override
     public void render(SpriteBatch batch, Tile tile) {
+        render(batch, tile, centerTexture);
+        drawBranches(batch, tile);
+    }
+
+    private void render(SpriteBatch batch, Tile tile, TextureRegion texture){
         var x = tile.getPixelX();
         var y = tile.getPixelY();
 
         batch.draw(texture, x, y);
-        drawBranches(tile);
     }
 
-    private void drawBranches(Tile tile) {
-        var pixelX = tile.getPixelX();
-        var pixelY = tile.getPixelY();
-        var shapeDrawer = Core.client.getGameShapeDrawer();
-
+    private void drawBranches(SpriteBatch batch, Tile tile) {
         if(isItemDuct(tile.getX(), tile.getY() + 1, tile.getWorld())){
-            shapeDrawer.filledRectangle(pixelX + 4, pixelY + 26, 22, 6, bottom);
-            shapeDrawer.filledRectangle(pixelX + 2, pixelY + 28, 2, 4, outerBorder);
-            shapeDrawer.filledRectangle(pixelX + 28, pixelY + 28, 2, 4, outerBorder);
-            shapeDrawer.filledRectangle(pixelX + 4, pixelY + 26, 2, 6, innerBorder);
-            shapeDrawer.filledRectangle(pixelX + 26, pixelY + 26, 2, 6, innerBorder);
-            shapeDrawer.filledRectangle(pixelX + 6, pixelY + 26, 20, 6, glassColor);
+            render(batch, tile, topTexture);
         }
-
         if(isItemDuct(tile.getX(), tile.getY() - 1, tile.getWorld())){
-            shapeDrawer.filledRectangle(pixelX + 4, pixelY, 22, 6, bottom);
-            shapeDrawer.filledRectangle(pixelX + 2, pixelY, 2, 4, outerBorder);
-            shapeDrawer.filledRectangle(pixelX + 28, pixelY, 2, 4, outerBorder);
-            shapeDrawer.filledRectangle(pixelX + 4, pixelY, 2, 6, innerBorder);
-            shapeDrawer.filledRectangle(pixelX + 26, pixelY, 2, 6, innerBorder);
-            shapeDrawer.filledRectangle(pixelX + 6, pixelY, 20, 6, glassColor);
+            render(batch, tile, bottomTexture);
         }
-
-
         if(isItemDuct(tile.getX() - 1, tile.getY(), tile.getWorld())){
-            shapeDrawer.filledRectangle(pixelX, pixelY + 6, 6, 20, bottom);
-            shapeDrawer.filledRectangle(pixelX, pixelY + 2, 4, 2, outerBorder);
-            shapeDrawer.filledRectangle(pixelX, pixelY + 28, 4, 2, outerBorder);
-            shapeDrawer.filledRectangle(pixelX, pixelY + 4, 4, 2, innerBorder);
-            shapeDrawer.filledRectangle(pixelX, pixelY + 26, 4, 2, innerBorder);
-            shapeDrawer.filledRectangle(pixelX, pixelY + 6, 6, 20, glassColor);
+            render(batch, tile, leftTexture);
         }
-
         if(isItemDuct(tile.getX() + 1, tile.getY(), tile.getWorld())){
-            shapeDrawer.filledRectangle(pixelX + 26, pixelY + 6, 6, 20, bottom);
-            shapeDrawer.filledRectangle(pixelX + 28, pixelY + 2, 4, 2, outerBorder);
-            shapeDrawer.filledRectangle(pixelX + 28, pixelY + 28, 4, 2, outerBorder);
-            shapeDrawer.filledRectangle(pixelX + 28, pixelY + 4, 4, 2, innerBorder);
-            shapeDrawer.filledRectangle(pixelX + 28, pixelY + 26, 4, 2, innerBorder);
-            shapeDrawer.filledRectangle(pixelX + 26, pixelY + 6, 6, 20, glassColor);
+            render(batch, tile, rightTexture);
         }
     }
 
@@ -97,11 +75,8 @@ public class ItemDuct extends Block implements BlockRenderer {
 
     @Override
     public void buildRender(SpriteBatch batch, Tile tile) {
-        var x = tile.getPixelX();
-        var y = tile.getPixelY();
-
         batch.setColor(1f, 1f, 1f, .5f);
-        batch.draw(texture, x, y);
+        render(batch, tile);
         batch.setColor(1f, 1f, 1f, 1f);
     }
 }
