c7c65e7ae5df0f449ecb17d77d6d57ca4ccecd73
Noah Charlton <ncharlton002@gmail.com>
2020-06-04 23:21:35 -0400

    Include position when entity added on client

--- core/src/com/noahcharlton/robogeddon/entity/NewEntityMessage.java
+++ core/src/com/noahcharlton/robogeddon/entity/NewEntityMessage.java
@@ -5,6 +5,7 @@ import com.noahcharlton.robogeddon.world.team.Team;
 
 public class NewEntityMessage implements Message {
 
+    private final EntityUpdateMessage info;
     private final String entityType;
     private final int ID;
     private final Team team;
@@ -13,6 +14,7 @@ public class NewEntityMessage implements Message {
         this.entityType = entity.getType().getTypeID();
         this.ID = entity.getId();
         this.team = entity.getTeam();
+        this.info = entity.createUpdateMessage();
     }
 
     public int getID() {
@@ -25,5 +27,9 @@ public class NewEntityMessage implements Message {
 
     public Team getTeam() {
         return team;
+    }
+
+    public EntityUpdateMessage getInfo() {
+        return info;
     }
 }
